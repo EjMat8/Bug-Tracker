acf28c2caed8ec2f97fa752800e38968f80a3114
EjMat8 <franzelijahmatugas@gmail.com>
2021-09-28 21:42:26 +0800

    updated error handling

--- backend/controllers/globalErrorController.js
+++ backend/controllers/globalErrorController.js
@@ -9,10 +9,15 @@ const sendErrorDev = (err, res) => {
   });
 };
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR", err);
+    res.status(500).json({ status: "error", message: "Something went wrong" });
+  }
 };
 
 const handleCastError = (err, statusCode) => {
@@ -36,9 +41,9 @@ module.exports = (err, req, res, _) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "development") {
+  } else if (process.env.NODE_ENV === "production") {
     let error = { ...err, name: err.name, message: err.message };
     if (error.name === "CastError") error = handleCastError(err, 400);
     if (error.code === 11000) error = handleDupilcateError(err, 400);
