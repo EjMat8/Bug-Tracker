6b3c86b41b4b49c43fa53a7100141074eea95a9e
EjMat8 <franzelijahmatugas@gmail.com>
2021-10-06 11:11:11 +0800

    db connect

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,20 @@ const morgan = require("morgan");
 
 const app = express();
 
-if (process.env.NODE_ENV === "default") app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use(express.json({ limit: "10kb" }));
 
 app.use(express.static(path.join(process.cwd(), "public")));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ status: "success", data: { something: "hi" } });
+// app.use("/api/users", userRoutes);
+
+app.use("*", (_, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Could not find what you are looking for",
+  });
+  next();
 });
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 process.on("uncaughtException", (err) => {
@@ -12,7 +13,16 @@ dotenv.config({ path: path.join(process.cwd(), "config.env") });
 const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
+const DB = process.env.DB.replace("<password>", process.env.DB_PASS);
 
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log("db err", err.message);
+  });
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
